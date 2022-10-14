72b6d8da50c1cf19df25424b20df200340d84524
Anabel <villalobos1998@gmail.com>
2022-10-14 09:37:21 -0700

    finished reverse string test

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,13 @@
 module.exports = function reverseString(string) {
-  // Your code here
+  if(typeof string !== 'string') {
+    throw new TypeError;
+  }
+
+  let newString = '';
+
+  for(let i = string.length - 1; i >= 0; i--) {
+    newString += string[i];
+  }
+
+  return newString;
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,27 @@
-// Your code here
+const chai = require('chai');
+const expect = chai.expect;
+
+const reverseString = require('../problems/reverse-string');
+
+
+describe('reverse-string', function() {
+
+        it('should return the string reversed', function() {
+            let input = 'fun';
+            let inputReversed = reverseString(input);
+            expect(inputReversed).to.equal('nuf');
+    
+        })
+        
+        it('should throw an error if input is not a string', function() {
+            let error = new Error('Input must be a string')
+            let number = 1;
+            let object = { a: 1 };
+            let array = [1];
+            expect(() => reverseString(number)).to.throw(TypeError);
+            expect(() => reverseString(object)).to.throw(TypeError);
+            expect(() => reverseString(array)).to.throw(TypeError);
+    
+    })
+})
+    
