488c07bbc7b088afc9ea9c52255476cb79cf994f
Keenly <keenlyhere@gmail.com>
2022-10-19 22:40:47 -0700

    first commit

--- problems/my-map.js
+++ problems/my-map.js
@@ -1,5 +1,18 @@
 function myMap(inputArray, callback) {
-  // Your code here
+  let result = [];
+
+  for (let i = 0; i < inputArray.length; i++) {
+    const element = inputArray[i];
+    result.push(callback(element))
+  }
+
+  return result;
 }
+
+// const arr = [1, 2, 3];
+// const callback = (el) => el * 2;
+
+// console.log(myMap(arr, callback)); // prints [2,4,6]
+// console.log(arr); // prints [1,2,3]
 
 module.exports = myMap;
--- test/my-map-spec.js
+++ test/my-map-spec.js
@@ -1,1 +1,43 @@
-// Your code here
+const { expect } = require("chai");
+const chai = require("chai");
+const spies = require("chai-spies");
+chai.use(spies);
+
+const myMap = require("../problems/my-map");
+
+
+
+
+describe("myMap", function() {
+
+    let array;
+
+    beforeEach(() => {
+        array = new Array(1, 2, 3)
+    });
+    it("should mimic the built-in Array.map", function() {
+        let result1 = myMap(array, (el) => el *= 2);
+        expect(result1).to.eql([2, 4, 6]);
+
+        let result2 = myMap([100, 25, 81, 64], Math.sqrt);
+        expect(result2).to.eql([10, 5, 9, 8]);
+    })
+
+    it("should not mutate the passed-in array argument", function() {
+        expect(array).to.eql([1, 2, 3]);
+    });
+
+    it("should not call the built-in Array.map", function() {
+        const mapSpy = chai.spy.on(Array.prototype, "map");
+        myMap(array, (el) => el *= 2);
+        expect(mapSpy).to.have.not.been.called();
+    });
+
+    it("should invoke the passed-in callback once for each element in the passed-in array argument")
+        // let cb = (el) => el *= 2;
+        let spy = chai.spy((el) => el *= 2)
+        myMap([1, 2, 3], spy)
+        expect(spy).to.have.been.called()
+
+
+});
